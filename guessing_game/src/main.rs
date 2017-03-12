f52ddbcb8fe4c779f5bed6abe5cd71b5271a198c
Sumit Srivastava <srivassumit@gmail.com>
2017-03-12 04:24:21 -0400

    guessing game part 1

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,24 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess The Number");
+
+    println!("Please input your Guess");
+
+// let keyword is used to assign variables.
+// rust variables are immutable by default.
+// the mut keyword tells that this variable is mutable
+    let mut guess = String::new();// new creates a new string.
+// the :: thing means that new is like a static method of String class.
+
+// read_line takes a reference to a mutable String as argument.
+// It then assigns the value from Std in to the mutable String reference passed.
+    io::stdin().read_line(&mut guess)
+                .expect("Failed to read line");
+// read_line returns a io:Result which encodes error handling info.
+// expect is a method of io::Result
+// on error, the program stops and displays the message given in expect method.
+//not giving expect wil give a warning at compile time.
+
+    println!("You guessed: {}", guess);
 }
