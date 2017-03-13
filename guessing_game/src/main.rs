3b740c72877979e5620a86e66086d4309d0c9a7e
Sumit Srivastava <srivassumit@gmail.com>
2017-03-13 04:21:47 -0400

    Guessing Game part 2

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,24 +1,44 @@
+extern crate rand;
+
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess The Number");
 
-    println!("Please input your Guess");
+    let secret_number = rand::thread_rng().gen_range(1,101);
 
-// let keyword is used to assign variables.
-// rust variables are immutable by default.
-// the mut keyword tells that this variable is mutable
-    let mut guess = String::new();// new creates a new string.
-// the :: thing means that new is like a static method of String class.
+    //println!("The secret number is:{}", secret_number);
+    loop {
+        println!("Please input your Guess");
 
-// read_line takes a reference to a mutable String as argument.
-// It then assigns the value from Std in to the mutable String reference passed.
-    io::stdin().read_line(&mut guess)
-                .expect("Failed to read line");
-// read_line returns a io:Result which encodes error handling info.
-// expect is a method of io::Result
-// on error, the program stops and displays the message given in expect method.
-//not giving expect wil give a warning at compile time.
+    // let keyword is used to assign variables.
+    // rust variables are immutable by default.
+    // the mut keyword tells that this variable is mutable
+        let mut guess = String::new();// new creates a new string.
+    // the :: thing means that new is like a static method of String class.
 
-    println!("You guessed: {}", guess);
+    // read_line takes a reference to a mutable String as argument.
+    // It then assigns the value from Std in to the mutable String reference passed.
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+    // read_line returns a io:Result which encodes error handling info.
+    // expect is a method of io::Result
+    // on error, the program stops and displays the message given in expect method.
+    //not giving expect wil give a warning at compile time.
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => {println!("You Win!");break;}
+        }
+    }
 }
